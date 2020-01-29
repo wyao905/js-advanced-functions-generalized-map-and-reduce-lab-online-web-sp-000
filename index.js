b76b5996a83acd706666410c016c939fff993593
Warren Yao <warrenyca905@gmail.com>
2020-01-29 01:24:27 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(array, fnc) {
+  let rt = []
+  for(let ele of array) {
+    rt.push(fnc(ele))
+  }
+  return rt
+}
+
+function reduce(array, fnc, start) {
+  let i = 0
+  if(!start) {
+    start = array[0]
+    i = 1
+  }
+  for(; i < array.length; i++) {
+    start = fnc(array[i], start)
+  }
+  return start
+}
